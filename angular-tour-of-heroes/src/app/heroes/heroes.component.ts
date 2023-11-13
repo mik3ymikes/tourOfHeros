6ecabdb915f8083d6d6782f8ce08b4c1bade90e6
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-13 14:08:36 -0600

    created dependency injection and injected it

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES  } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
+
+
+
 export class HeroesComponent {
-  heroes=HEROES
+  constructor(private heroservice:HeroService){}
+  heroes:Hero[]=[]
   selectedHero?: Hero
 
 hero: Hero={
