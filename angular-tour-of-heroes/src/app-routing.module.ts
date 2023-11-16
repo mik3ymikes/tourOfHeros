6576930aa2d1dc3b15238f6224a2b1c2629bdcd4
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-16 14:15:22 -0600

    completed dashboardcomponent hero links

--- angular-tour-of-heroes/src/app-routing.module.ts
+++ angular-tour-of-heroes/src/app-routing.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { HeroDetailComponent } from './app/hero-detail/hero-detail.component';
 
 import { RouterModule, Routes } from '@angular/router';
 import { HeroesComponent } from './app/heroes/heroes.component';
@@ -8,7 +9,8 @@ import { DashboardComponent } from './app/dashboard/dashboard.component';
 const routes:Routes=[
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
   {path:'heroes', component:HeroesComponent},
-  {path:'dashboard', component:DashboardComponent}
+  {path:'dashboard', component:DashboardComponent},
+  {path:'detail/id', component: HeroDetailComponent}
 ]
 
 @NgModule({
