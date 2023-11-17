7292c64b27dfe315ec7db4ccd096f8dc75a6c022
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-16 19:35:25 -0600

    fixed bug that was stopping routing to hero id

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,30 +1,27 @@
-import { Component, Input } from '@angular/core';
-import { Hero } from '../hero';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+
+import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { OnInit } from '@angular/core';
-
-
 
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css']
+  styleUrls: [ './hero-detail.component.css' ]
 })
+export class HeroDetailComponent implements OnInit {
+  hero: Hero | undefined;
 
-
-
-
-export class HeroDetailComponent implements OnInit{
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
   ngOnInit(): void {
     this.getHero();
   }
-
-  constructor(private route:ActivatedRoute,
-    private heroService:HeroService,
-    private location:Location){}
 
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
@@ -32,8 +29,7 @@ export class HeroDetailComponent implements OnInit{
       .subscribe(hero => this.hero = hero);
   }
 
-
-
-
-  @Input() hero? : Hero
+  goBack(): void {
+    this.location.back();
+  }
 }
--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -1,52 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { OnInit } from '@angular/core';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
-
-
-
 export class HeroesComponent implements OnInit {
-  heroes:Hero[]=[]
-  constructor(private heroservice:HeroService){}
-    // private meessageService:MessageService){}
+  heroes: Hero[] = [];
 
-
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
-    this.getHeros();
+    this.getHeroes();
   }
 
-
-  // heroes:Hero[]=[]
-  // selectedHero?: Hero
-
-
-getHeros():void{
- this.heroservice.getHeroes()
- .subscribe(heroes =>{
-  this.heroes=heroes
- })
-}
-
-
-
-// onSelect(hero: Hero){
-//   this.selectedHero=hero
-//   this.meessageService.add('HeoresComponent:Selected hero id=' + hero.id)
-//   console.log(hero)
-//   // this.selectedHero=hero
-// }
-
-
-
-
-
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 }
