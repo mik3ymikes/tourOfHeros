a4962cf0f4ff311b227264f41c851995a1a8b395
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-19 13:41:54 -0600

    added a handle error obsrevable

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -19,9 +19,27 @@ export class HeroService {
   this.messageService.add('HeroService' +message)
 }
 
+
+private handleError<T>(operation='operation', result?:T){
+  return(error:any):Observable<T> =>{
+     // TODO: send the error to remote logging infrastructure
+     console.error(error); // log to console instead
+
+     // TODO: better job of transforming error for user consumption
+     this.log(`${operation} failed: ${error.message}`);
+
+     // Let the app keep running by returning an empty result.
+     return of(result as T);
+
+  }
+}
+
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
-  }
+    .pipe(
+      catchError(this.handleError<Hero[]>('getHeroes', []))
+  )
+}
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
