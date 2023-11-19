aa4b01066db89ffc42674cacc8f9aa0062e23b3e
mik3ymikes <113144829+mik3ymikes@users.noreply.github.com>
2023-11-19 13:53:42 -0600

    updated the hero service to make url request and catch and log error if needed

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -37,15 +37,21 @@ private handleError<T>(operation='operation', result?:T){
   getHeroes(): Observable<Hero[]> {
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(_=>this.log('fetched heroes')),
       catchError(this.handleError<Hero[]>('getHeroes', []))
   )
 }
 
+
+
   getHero(id: number): Observable<Hero> {
-    // For now, assume that a hero with the specified `id` always exists.
-    // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(hero);
-  }
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+}
+
+
+
 }
